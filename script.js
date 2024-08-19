f61b61f3b95da990aa7e7e44680ab0066f37e17a
aaronrzt <aaronrzt@outlook.com>
2024-08-18 21:54:34 -0600

    [Fix] Spacing for list entries is now uniform.

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', () =>
         const titleBar = document.getElementById('title-bar');
     
         const normalPrompt = 'user@cv-aaronrzt:~$ ';
-        const selectionPrompt = 'Selection: ';
+        const selectionPrompt = 'Select a project [0...3], 8 to see the list or 9 to cancel: ';
         let isInSelectionMode = false;
     
         function displayPrompt(promptText = normalPrompt) {
@@ -77,60 +77,66 @@ document.addEventListener('DOMContentLoaded', () =>
                         output.innerHTML += `<div>${selectionPrompt}${command}</div>`;
                         output.innerHTML += `<div>Exiting project selection.</div>`;
                         displayPrompt();
-                    } else if (['0', '1', '2', '3'].includes(command)) {
+                    } else if (['0', '1', '2', '3', '8'].includes(command)) {
                         output.innerHTML += `<div>${selectionPrompt}${command}</div>`;
                         switch(command) {
                             case '0':
                                 output.innerHTML += `<div>
 Navigation with Obstacle Avoidance for Manchester Robotics
-February – June 2023
-Developed Intelligent Robotics for Navigation with Obstacle Avoidance using Python in ROS.
-    <ul class="padded-list">
-        <li>Developed with Python in ROS an intelligent navigation system, which used obstacle avoidance.</li>
-        <li>Tuned a Kalman Filter for precise state estimations, being aware of noise and uncertainties.</li>
-        <li>Implemented Bug Algorithms for precision in obstacle avoidance.</li>
-        <li>Validated through Gazebo simulations and deployed on the Jetson Nano.</li>
-    </ul>
+<br>February – June 2023
+<br>Developed Intelligent Robotics for Navigation with Obstacle Avoidance using Python in ROS.<div class="padded-list">
+-Developed with Python in ROS an intelligent navigation system, which used obstacle avoidance.
+<br>-Tuned a Kalman Filter for precise state estimations, being aware of noise and uncertainties.
+<br>-Implemented Bug Algorithms for precision in obstacle avoidance.
+<br>-Validated through Gazebo simulations and deployed on the Jetson Nano.
+</div>
                         </div>`;
                                 break;
                             case '1':
                                 output.innerHTML += `<div>
 Industrial Internet of Things (IIoT) Project for Laser & Manufacturing
-September – December 2023
-Developed a Python-based platform for monitoring an industrial manufacturing process.
-    <ul class="padded-list">
-        <li>Implemented a system using Node-RED and Python in a Raspberry Pi to monitor physical variables.</li>
-        <li>Analyzed the data and used it to display relevant information in a user-friendly dashboard to aid decision-making.</li>
-    </ul>
+<br>September – December 2023
+<br>Developed a Python-based platform for monitoring an industrial manufacturing process.<div class="padded-list">
+-Implemented a system using Node-RED and Python in a Raspberry Pi to monitor physical variables.
+<br>-Analyzed the data and used it to display relevant information in a user-friendly dashboard to aid decision-making.
+</div>
                         </div>`;
                                 break;
                             case '2':
                                 output.innerHTML += `<div>
 Autonomous Vehicle Navigation for Manchester Robotics and NVIDIA
-March – June 2022
-Developed Intelligent Robotics for Autonomous Vehicle Navigation using Python in ROS.
-    <ul class="padded-list">
-        <li>Developed and fine-tuned a neural network model for accurate traffic sign recognition and decision-making.</li>
-        <li>Applied advanced Computer Vision methods for high-precision line detection using OpenCV.</li>
-        <li>Validated through Gazebo simulations and deployed on the Jetson Nano.</li>
-    </ul>
+<br>March – June 2022
+<br>Developed Intelligent Robotics for Autonomous Vehicle Navigation using Python in ROS.<div class="padded-list">
+-Developed and fine-tuned a neural network model for accurate traffic sign recognition and decision-making.
+<br>-Applied advanced Computer Vision methods for high-precision line detection using OpenCV.
+<br>-Validated through Gazebo simulations and deployed on the Jetson Nano.
+</div>
                         </div>`;
                                 break;
                             case '3':
                                 output.innerHTML += `<div>
 Audio Player in ATMega2560
-January – June 2021
-Developed a .wav file player, writing C code directly into an ATMega2560 chip.
-    <ul class="padded-list">
-        <li>Designed and tested hardware through simulations in Proteus 8 before assembling it.</li>
-        <li>Thoroughly tested the code to validate the system’s performance using files of varying size.</li>
-        <li>Utilized the chip’s internal clock to ensure precise timing for each instruction.</li>
-    </ul>
+<br>January – June 2021
+<br>Developed a .wav file player, writing C code directly into an ATMega2560 chip.<div class="padded-list">
+-Designed and tested hardware through simulations in Proteus 8 before assembling it.
+<br>-Thoroughly tested the code to validate the system’s performance using files of varying size.
+<br>-Utilized the chip’s internal clock to ensure precise timing for each instruction.
+</div>
                         </div>`;
                                 break;
+                                case '8':
+                                    output.innerHTML += `<div>
+Type [0...3] to select a project and read its details or type 9 to cancel.
+
+0. Navigation with Obstacle Avoidance for Manchester Robotics
+1. Industrial Internet of Things (IIoT) Project for Laser & Manufacturing
+2. Autonomous Vehicle Navigation for Manchester Robotics and NVIDIA
+3. Audio Player in ATMega2560
+
+    </div>`;
+                                    break;
                             default:
                                 output.innerHTML += `<div>${selectionPrompt}${command}</div>`;
-                                output.innerHTML += `<div>Retard Invalid selection. Type [0...3] to select a project or 9 to cancel.</div>`;
                                 break;
                         }
                     } else {
@@ -152,6 +158,7 @@ These commands are defined internally.  Type 'help' to see this list.
 clear
 education
 help
+contact
 leadership
 merits
 projects
@@ -162,28 +169,33 @@ work
                         output.innerHTML += `<div>
 ITESM, Campus Querétaro, México
 August 2019 – June 2023
-B.S., Major in Digital Systems and Robotics Engineering
-    <ul class="padded-list">
-        <li>Minor in 4.0 Systems and Technologies</li>
-        <li>Grade: 92/100, GPA: 3.7</li>
-        <li><a href="https://certificados.tec.mx/certificate/828e105a490957388d37b5d6009525f6" target="_blank" style="color: #fff;">Certificate</a></li>
-    </ul>
+    B.S., Major in Digital Systems and Robotics Engineering
+    Minor in 4.0 Systems and Technologies
+    Grade: 92/100, GPA: 3.7
+    <a href="https://certificados.tec.mx/certificate/828e105a490957388d37b5d6009525f6" target="_blank" style="color: #fff;">Certificate</a>
+                        </div>`;
+                    } else if (command.toLowerCase() === 'contact'){
+                        output.innerHTML += `<div>
+Aarón Gonzalo Ramírez Tafolla
+    +52 462 146 08 59
+    aaronrzt@outlook.com
+    <a href="https://github.com/aaronrzt/" target="_blank" style="color: #fff;">https://github.com/aaronrzt/</a>
+    <a href="https://www.linkedin.com/in/aaronrzt/" target="_blank" style="color: #fff;">https://www.linkedin.com/in/aaronrzt/</a>
                         </div>`;
                     } else if (command.toLowerCase() === 'leadership'){
                         output.innerHTML += `<div>
 Vortex, 2023 VEX Robotics World Championship
-August 2022 – April 2023
-Development of two robots to compete in the Spin Up VEX Challenge.
-    <ul class="padded-list">
-        <li>Engineered and constructed robots with a focus on speed and functionality for competition settings.</li>
-        <li>Implemented algorithms in C++ for autonomous and control-driven programs, used control systems and input data collected from sensors.</li>
-    </ul>
+<br>August 2022 – April 2023
+<br>Development of two robots to compete in the Spin Up VEX Challenge. <div class="padded-list">
+- Engineered and constructed robots with a focus on speed and functionality for competition settings.
+<br>- Implemented algorithms in C++ for autonomous and control-driven programs, used control systems and input data collected from sensors.
+</div>
                         </div>`;
                     } else if (command.toLowerCase() === 'merits'){
-                        output.innerHTML += `<div>
-2023 – 1st place in Expo Ingenierías with IIoT project
-2023 – Judges Champions in National VEX Robotics Competition
-2022 – Participation in Expo Ingenierías with a vehicle with autonomous navigation
+                        output.innerHTML += `<div><div class="padded-list">
+<br>2023 – 1st place in Expo Ingenierías with IIoT project.
+<br>2023 – Judges Champions in National VEX Robotics Competition.
+<br>2022 – Participation in Expo Ingenierías with a vehicle with autonomous navigation. </div>
                         </div>`;
                     } else if (command.toLowerCase() === 'projects') {
                         isInSelectionMode = true;
@@ -207,19 +219,17 @@ Git, Linux, ROS, Gazebo, OpenCV, CUDA, TensorFlow, PyTorch, Keras, Pandas, Excel
                     } else if (command.toLowerCase() === 'work'){
                         output.innerHTML += `<div>
 B&R (ABB Group), Application Engineer
-July 2023 – January 2024
-Software development for industrial manufacturing applications (contract work).
-    <ul class="padded-list">
-        <li>Planned and developed the logic behind manufacturing processes using Structured Text and Ladder Logic.</li>
-        <li>Planned and taught courses on proprietary technologies.</li>
-        <li>Communicated directly with clients to work out project requirements and provided customer support (hardware and software).</li>
-    </ul>
-General Electric Aerospace, Software Verification Intern
-August 2022 – January 2023
-Unit Testing for software modules destined for aerospace engines.
-    <ul class="padded-list">
-        <li>Designed and implemented unit test scripts in VBA and Python to automate verification tasks.</li>
-    </ul>
+<br>July 2023 – January 2024
+<br>Software development for industrial manufacturing applications (contract work).<div class="padded-list">
+-Planned and developed the logic behind manufacturing processes using Structured Text and Ladder Logic.
+<br>-Planned and taught courses on proprietary technologies.
+<br>-Communicated directly with clients to work out project requirements and provided customer support (hardware and software).
+</div>
+<br>General Electric Aerospace, Software Verification Intern
+<br>August 2022 – January 2023
+<br>Unit Testing for software modules destined for aerospace engines.<div class="padded-list">
+-Designed and implemented unit test scripts in VBA and Python to automate verification tasks.
+</div>
                         </div>`;
                     } else {
                         output.innerHTML += `<div>${command}: command not found. Type 'help' for a list of commands.</div>`;
@@ -231,6 +241,6 @@ Unit Testing for software modules destined for aerospace engines.
                 }
             }
         });
-    
+        output.innerHTML += `<div>Welcome! Type 'help' to get started.</div>`;
         displayPrompt();
     });
